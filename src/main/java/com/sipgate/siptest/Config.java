ae3fccf0da9848f1ef9fa8a0316cfecf4ae4ddff
Alexander Kluth <deralex@cpan.org>
2019-02-15 01:30:59 +0100

    Implement configuration files based on TOML in order to load the password and number
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

---
+++ src/main/java/com/sipgate/siptest/Config.java
@@ -0,0 +1,12 @@
+package com.sipgate.siptest;
+
+public class Config {
+
+    public String password;
+
+    public String number;
+
+    public Config(String configFile) {
+
+    }
+}
--- src/main/java/com/sipgate/siptest/Main.java
+++ src/main/java/com/sipgate/siptest/Main.java
@@ -2,8 +2,11 @@ package com.sipgate.siptest;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+import com.moandjiezana.toml.Toml;
 import com.sipgate.type.extension.E;
 import com.sipgate.type.extension.Extension;
+
+import java.io.File;
 import java.nio.file.Paths;
 import org.slf4j.Logger;
 
@@ -12,14 +15,15 @@ public class Main {
 	private static final Logger LOGGER = getLogger(Main.class);
 
 	private static final E EXTENSION_0 = (E) Extension.parse("2477148e0").get();
-	private static final String PASSWORD_0 = "W6oaVgRhdY4H";
 	private static final String NUMBER_0 = "020387844335";
 
 	public static void main(String[] args) {
+		Config config = getConfig();
+
 		final SipgateUserAgent userAgent = SipgateUserAgent
 				.newBuilder()
 				.forExtension(EXTENSION_0)
-				.withSecret(PASSWORD_0)
+				.withSecret(config.password)
 				.withName("Caller")
 				.withAudioOutput(Paths.get("/tmp/audio.out"))
 				.build();
@@ -40,5 +44,26 @@ public class Main {
 		LOGGER.info("Registering");
 
 		userAgent.register();
+	}
+
+	/**
+	 * Load config.toml configuration file and create new Config instance
+	 *
+	 * @return Config instance initialized with contents from config.toml
+	 */
+	private static Config getConfig() {
+		String filePath = "";
+
+		try {
+			filePath = Main.class.getClassLoader().
+					getResource("config.toml").
+					getFile();
+		} catch (NullPointerException e) {
+			LOGGER.error("Could not find config.toml", e);
+			System.exit(-1);
+		}
+
+		File configFile = new File(filePath);
+		return new Toml().read(configFile).to(Config.class);
 	}
 }
